d0a86eb3bfba248e4b93413016fa8977046464c1
Ahmed Hammam <ahmedhammam710@gmail.com>
2022-08-30 17:27:49 +0200

    add user athentication

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,9 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',  #new
     #3rd party
     'rest_framework',
     'corsheaders', 
+    'rest_framework.authtoken',
+    'allauth',   #new
+    'allauth.account',  #new
+    'allauth.socialaccount',   #new
+    'dj_rest_auth',
+    'dj_rest_auth.registration',   #new
     #local 
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig', 
@@ -69,10 +76,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',  #new
             ],
         },
     },
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"   #new
+
+SITE_ID = 1  #new 
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
@@ -135,6 +147,10 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_AUTHENTICATION_CLASSES":[
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
 }
 
 CORS_ORIGIN_WHITELIST = (
